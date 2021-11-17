e7126d661edb47e730e673cbdeb00e1867e1d0c8
LDHugeman <luisdavid.vp@gmail.com>
2021-11-17 10:58:12 +0100

    Clase 16

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -5,6 +5,8 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
-
+    Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
     private ComponentDependency componentDependency;
     private MyBean myBean;
     private MyBeanWithDependency myBeanWithDependency;
@@ -44,5 +46,12 @@ public class FundamentosApplication implements CommandLineRunner {
         myBeanWithDependency.printWithDependency();
         System.out.println(myBeanWithProperties.function());
         System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());
+        try {
+            int value = 10 / 0;
+            LOGGER.debug("Mi valor: " + value);
+        } catch (Exception e) {
+            LOGGER.error("Esto es un error al dividir por cero " + e.getStackTrace());
+        }
+
     }
 }
